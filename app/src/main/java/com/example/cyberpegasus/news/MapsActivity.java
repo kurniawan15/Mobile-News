f1471e263802569b383cdf99ea765108f6f6c063
gerryagustian97 <31478259+gerryagustian97@users.noreply.github.com>
2018-07-24 16:57:03 +0700

    Reverse geocode hardcode

--- app/src/main/java/com/example/cyberpegasus/news/MapsActivity.java
+++ app/src/main/java/com/example/cyberpegasus/news/MapsActivity.java
@@ -1,12 +1,32 @@
 package com.example.cyberpegasus.news;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,11 +34,33 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity extends AppBaseActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    Button btnFinish;
+    Button btnFinish, btnGetAddress;
     EditText txtAddress;
+    TextView tv;
+    Float zoom = 16.0f;
+    LocationManager locationManager;
+    String provider;
+    double lat, lng;
+    final int MY_PERMISSION_REQUEST_CODE = 7171;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    getLocation();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +80,25 @@ public class MapsActivity extends AppBaseActivity implements OnMapReadyCallback 
                 Intent formIntent = new Intent(MapsActivity.this, FormActivity.class);
                 formIntent.putExtra("ADDRESS", address);
                 startActivity(formIntent);
+            }
+        });
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                },MY_PERMISSION_REQUEST_CODE);
+        } else {
+            getLocation();
+        }
+
+        btnGetAddress = (Button) findViewById(R.id.buttonGetAddress);
+        btnGetAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lat = -6.863257;
+                lng = 107.584040;
+                new GetAddress().execute(String.format("%.4f,%.4f",lat,lng));
             }
         });
     }
@@ -61,12 +122,90 @@ public class MapsActivity extends AppBaseActivity implements OnMapReadyCallback 
         LatLng sembada = new LatLng(-6.863257, 107.584040);
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sembada));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sembada, 12.0f));
-        mMap.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sembada, zoom));
+        /*mMap.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
             @Override
             public void onCameraIdle() {
                 LatLng midLatLng = mMap.getCameraPosition().target;
+                String[] midLatLngStr = midLatLng.toString().split(",");
+                lat = Double.parseDouble(midLatLngStr[0]);
+                lng = Double.parseDouble(midLatLngStr[1]);
             }
-        });
+        });*/
+    }
+
+    public void geoLocate(View view) throws IOException {
+        String location = txtAddress.getText().toString();
+
+        Geocoder gc = new Geocoder(this);
+        try {
+            List<Address> list = gc.getFromLocationName(location, 1);
+            Address address = list.get(0);
+            String locality = address.getLocality();
+
+            double lat = address.getLatitude();
+            double lng = address.getLongitude();
+            LatLng ll = new LatLng(lat, lng);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ll, zoom));
+        } catch (NullPointerException ne) {
+            Toast.makeText(this, "Alamat tidak ditemukan !", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private void getLocation() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        final Location location = locationManager.getLastKnownLocation(provider);
+        if(location == null)
+            Log.e("ERROR", "Location is null");
+    }
+
+    private class GetAddress extends AsyncTask<String,Void,String>{
+        ProgressDialog dialog = new ProgressDialog(MapsActivity.this);
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("Please wait...");
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                double lat = Double.parseDouble(strings[0].split(",")[0]);
+                double lng = Double.parseDouble(strings[0].split(",")[1]);
+                String response;
+                HttpDataHandler http = new HttpDataHandler();
+                String url = String.format("https://maps.googleapis.com/maps/api/geocode/json?latlng=%.4f,%.4f&sensor=false", lat, lng);
+                response = http.GetHttpData(url);
+                return response;
+            }catch (Exception e){
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            try{
+                JSONObject jsonObject = new JSONObject(s);
+                String address = ((JSONArray)jsonObject.get("results")).getJSONObject(0).get("formatted_address").toString();
+                //tv.setText(address);
+                txtAddress.setText(address);
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+
+            if(dialog.isShowing()){
+                dialog.dismiss();
+            }
+        }
     }
 }
