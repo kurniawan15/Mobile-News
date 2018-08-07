b37426e5985ecce7d4dcec7c540a2500a2c2d6c0
EDMPL <akaedmpl@gmail.com>
2018-08-07 12:01:00 +0700

    LUl

--- app/src/main/java/com/example/cyberpegasus/news/network/RetrofitInstance.java
+++ app/src/main/java/com/example/cyberpegasus/news/network/RetrofitInstance.java
@@ -10,7 +10,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitInstance {
 
     private static Retrofit retrofit = null;
-    private static final String BASE_URL = "http://192.168.1.241:9099/api";
+    private static final String BASE_URL = "http://192.168.1.241:9099/api/";
 
     private RetrofitInstance() {}
 
